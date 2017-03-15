38b6657fe93750a14150c22beb4494f6589bfc67
tonysunyueran <yueran1@ualberta.ca>
2017-03-14 19:25:55 -0600

    add psycopg2

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import User, Role, Post, Comment, Permission
+from app.models import User, Role, Post, Comment, Permission, Follow
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -11,7 +11,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-	return dict(app=app, db=db, User=User, Role=Role, Post=Post, Comment=Comment, Permission=Permission)
+	return dict(app=app, db=db, User=User, Role=Role, Post=Post, Comment=Comment, Permission=Permission, Follow=Follow)
 	
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
